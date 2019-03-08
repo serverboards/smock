71de8957b79549b7601f653058a8934ec7d4d063
David Moreno <dmoreno@coralbits.com>
2019-03-08 19:43:05 +0100

    Updated version and set docs

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
 from distutils.core import setup
+import pathlib
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
 setup(
-  name = 'smock',
-  packages = ['smock'], # this must be the same as the name above
-  version = '0.2.0',
-  description = 'Serverboards Mock library',
-  author = 'David Moreno',
-  author_email = 'dmoreno@serverboards.io',
-  url = 'https://github.com/serverboards/smock', 
-  download_url = 'https://github.com/serverboards/smock/archive/0.1.tar.gz', 
-  keywords = ['testing', 'mock', 'mocking'], 
-  install_requires = ["PyYAML"],
-  classifiers = [],
+  name='smock',
+  packages=['smock'],  # this must be the same as the name above
+  version='0.2.2',
+  description='Serverboards Mock library',
+  author='David Moreno',
+  author_email='dmoreno@serverboards.io',
+  url='https://github.com/serverboards/smock',
+  download_url='https://github.com/serverboards/smock/archive/0.1.tar.gz',
+  keywords=['testing', 'mock', 'mocking'],
+  install_requires=["PyYAML"],
+  classifiers=[],
+  license="Apache2",
+  long_description=README,
+  long_description_content_type="text/markdown",
 )
